0c90faf61240cf6ef8a4f6738df89a89c204e60c
Daniel Ly <nalply@gmail.com>
2011-10-27 10:19:55 +0200

    Colorful ChunkStreamChumper understands SET CHUNK SIZE.

--- examples/ChunkStreamDumper.js
+++ examples/ChunkStreamDumper.js
@@ -7,10 +7,12 @@ var ChunkStream = mtrude.rtmp.ChunkStream;
 var asSocket = mtrude.asSocket;
 
 console.log(''
-  + 'Note: ChunkStream is not complete without MessageStream and might not\n'
-  + 'always work correctly (for example mis-synchronization due to a missing\n'
-  + 'implementation of "Set Chunk Size" (RTMP Message Formats 5.1).\n'
-  + 'Consider using MessageStreamDumper.js instead.\n'
+  + 'NOTE! ChunkStream is not complete without MessageStream and might get\n'
+  + 'stuck because of missing coordination with peer. Dump format:\n'
+  + 'CHUNK: %s %s %s %s %s:%s %s:%s,%s\n',
+  'data-1st-8-bytes'.blue, 'ascii-8c'.yellow,
+  'tstamp'.blue, 'ti'.green, 'msg-id'.blue, 'chs-id'.blue,
+  'msglen'.magenta, 'chklen'.blue, 'rest'.blue
 );
 
 if (process.argv[2] == '--debug') {
@@ -49,7 +51,8 @@ function dumpChunks(socket) {
     chunkStream.close();
   });
   chunkStream.on('end', function(graceful) {
-    console.log('END  :', 'Ended', (graceful ? '' : 'not ') + 'gracefully');
+    var msg = 'END  : Ended ' + (graceful ? '' : 'not ') + 'gracefully';
+    console.log(graceful ? msg.green : msg.red);
   });
   chunkStream.on('handshake', function() {
     console.log('HANDS: %s'.green, 'handshake completed successfully');
@@ -59,10 +62,40 @@ function dumpChunks(socket) {
   });
   chunkStream.on('chunk', function(chunk) {
     console.log(
-      'CHUNK: %s:%s typeid=%s chunk=%s data=%s rest=%s'.blue, chunk.csid,
-      chunk.msid, chunk.typeid, chunk.length, chunk.data.length, chunk.rest);
+      'CHUNK: %s %s %s %s %s:%s %s:%s,%s',
+      dump8(chunk.data).blue, ascii8(chunk.data),
+      hex6(chunk.timestamp).blue, hex2(chunk.typeid).green,
+      hex6(chunk.csid).blue, hex6(chunk.msid).blue,
+      chunk.length.toString().magenta, chunk.data.length.toString().blue,
+      chunk.rest.toString().blue);
+    if (chunk.typeid == 1) {
+      var chunkSize = chunk.data.readUInt32LE(0);
+      chunkStream.warn('ChunkStreamDumper setting chunk size to ' + chunkSize);
+      chunkStream.chunkSize = chunkSize;
+    }
   });
 }
 
+function dump8(data) {
+  function b(i) { return i < data.length ? hex2(data.readUInt8(i)) : '  '; }
 
+  return b(0) + b(1) + b(2) + b(3) + b(4) + b(5) + b(6) + b(7);
+}
+
+function hex2(byte) {
+  function hex1(nybble) { return "0123456789abcdef"[nybble & 0xf]; }
+  return hex1(byte >> 4) + hex1(byte);
+}
+
+function hex6(int24) {
+  return hex2(int24 >> 16) + hex2(int24 >> 8) + hex2(int24);
+}
+
+function ascii8(data) {
+  function asChar(b) { return String.fromCharCode(b); }
+  function ascii(b) { return b > 31 && b < 128 ? asChar(b).yellow : '·'.black; }
+  function b(i) { return i < data.length ? ascii(data.readUInt8(i)) : '·'.white; }
+
+  return b(0) + b(1) + b(2) + b(3) + b(4) + b(5) + b(6) + b(7);
+}
 
